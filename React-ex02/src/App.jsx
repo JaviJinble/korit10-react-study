a034a00dc5aa027581080e62a6cbe9270d90960b
JaviJinble <bubaragi23@gmail.com>
2026-05-07 04:45:10 +0900

    리엑트 ex01, ex02 2-1 질문 주석 추가

--- React-ex02/src/App.jsx
+++ React-ex02/src/App.jsx
@@ -3,6 +3,9 @@ import { useState, useEffect } from 'react'
 function App() {
   const [count, setCount] = useState(0);
 
+// currentId를 증가시키고 useEffect에서 리스트를 추가하는 구조가
+// 그냥 버튼에서 바로 추가하는 것보다 어떤 장점이 있는지 궁금합니다
+
   useEffect(() => {
     if (count !== 0) {
       console.log("현재 카운트:", count);
